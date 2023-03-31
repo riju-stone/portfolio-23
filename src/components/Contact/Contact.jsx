bab4d2ee61074e635d29f935d42044cedcff98e8
Arighna Chakraborty <riju23chakra@gmail.com>
2023-03-31 09:40:40 +0530

    initial commit

--- src/components/Contact/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { ContactSection } from "../../styles/contactStyles";
-
-function Contact() {
-	return <ContactSection id="contact"></ContactSection>;
-}
-
-export default Contact;
