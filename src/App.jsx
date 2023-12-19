2f8aeb1648f98dd1193ddeb055dd8f8e07306008
Arighna Chakraborty <riju23chakra@gmail.com>
2023-12-19 12:57:19 +0530

    removed vercel insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import GrowingCircle from "./components/background/GrowingCirle";
 import Cursor from "./components/cursor/Cursor";
@@ -37,7 +36,6 @@ function App() {
             </>
           )}
         </AnimatePresence>
-        <SpeedInsights />
       </div>
     </Router>
   );
